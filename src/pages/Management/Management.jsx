4c3f7c3784d840e57cd04831eb417532cacb497b
diegonacif <diegoreisnacif@gmail.com>
2023-02-01 23:44:52 -0300

    feat: register user in firestore with data from inputs

--- src/pages/Management/Management.jsx
+++ src/pages/Management/Management.jsx
@@ -1,8 +1,9 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, doc, getDocs, setDoc } from "firebase/firestore";
 import { useContext, useEffect, useState } from "react";
 import { AuthGoogleContext } from "../../contexts/AuthGoogleProvider";
 import { db } from '../../services/firebase';
 import { useCollection } from 'react-firebase-hooks/firestore';
+import { useForm } from 'react-hook-form';
 
 export const Management = () => {
   const usersCollectionRef = collection(db, "users");
@@ -10,8 +11,19 @@ export const Management = () => {
   const [alreadyRegistered, setAlreadyRegistered] = useState(false)
   const [firestoreLoading, setFirestoreLoading] = useState(true);
   const { userId } = useContext(AuthGoogleContext);
+  const [refresh, setRefresh] = useState(false);
 
   const IdsArray = firestoreLoading ? [] : users?.map((user) => user.id)
+
+  // Hook Form Controller
+  const {
+    watch,
+    register,
+    setValue,
+    getValues
+  } = useForm({
+    mode: "all"
+  });
   
   // Firestore loading
   const [value, loading, error] = useCollection(usersCollectionRef,
@@ -28,7 +40,7 @@ export const Management = () => {
       setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     }
     getUsers();
-  }, [])
+  }, [refresh])
 
    // Already Exists ?
   useEffect(() => {
@@ -42,17 +54,55 @@ export const Management = () => {
     }
   }, [IdsArray])
 
-  console.log({
-    "registered": alreadyRegistered,
-    "loading": firestoreLoading,
-    "id": userId
-  });
+  // console.log({
+  //   "registered": alreadyRegistered,
+  //   "loading": firestoreLoading,
+  //   "id": userId
+  // });
+
+  // Create a new user in firestore
+  async function registerUser() {
+    const docRef = doc(db, "users", userId);
+
+    return await setDoc(docRef, {
+      displayName: watch("displayName"),
+      phone: watch("phoneNumber"),
+      category: watch("vehicleCategory")
+    })
+    .then(
+      setRefresh((current) => !current),
+      console.log("Registered")
+    )
+  }
 
   // console.log(users);
 
   return (
     <div className="management-container">
       <h1>Management</h1>
+      {
+        alreadyRegistered ?
+        <span>Edição de dados</span> :
+        <>
+          <input 
+            type="text" 
+            placeholder="nome..." 
+            {...register("displayName")}
+          />
+          <input 
+            type="text" 
+            placeholder="telefone..." 
+            {...register("phoneNumber")}
+          />
+          <input 
+            type="text" 
+            placeholder="tipo de veículo..." 
+            {...register("vehicleCategory")}
+          />
+          
+          <button onClick={() => registerUser()}>Registrar</button>
+        </>
+      }
     </div>
   )
 }
