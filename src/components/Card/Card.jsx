c44273659efeb707e9f094bf83911fc7a74e1d35
diegonacif <diegoreisnacif@gmail.com>
2023-02-03 22:50:33 -0300

    feat: card component rendering data from users

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,23 +1,33 @@
 import personImg from '../../assets/man-person-icon.png';
+import placeholderImg from '../../assets/image-placeholder.png';
 
-export const Card = () => {
+export const Card = ({ displayName, phone, category, covered }) => {
   return (
     <div className="card-container">
-      <div className="card-row">
-        <img src={personImg} alt="" />
-        <span> João da Silva</span>
+      <div className="card-img-wrapper">
+        <img className="card-img" src={placeholderImg} alt="" />
       </div>
-      <div className="card-row">
-        <span>Telefone: </span>
-        <span>(84) 991751414</span>
-      </div>
-      <div className="card-row">
-        <span>Tamanho do veículo: </span>
-        <span>Médio</span>
-      </div>
-      <div className="card-row">
-        <span>É coberto: </span>
-        <span>Não</span>
+      <div className="card-info-wrapper">
+        <div className="card-row">
+          <span>{displayName}</span>
+          <img className="person-img" src={personImg} alt="" />
+        </div>
+        <div className="card-row">
+          <span>Telefone:</span>
+          <span>{phone}</span>
+        </div>
+        <div className="card-row">
+          <span>Tamanho do veículo:</span>
+          <span>{category}</span>
+        </div>
+        <div className="card-row">
+          <span>É coberto:</span>
+          <span>{covered}</span>
+        </div>
+        <div className="card-row">
+          <span>Outras informações:</span>
+          <span>blah</span>
+        </div>
       </div>
     </div>
   )
--- src/components/CardsContainer/CardsContainer.jsx
+++ src/components/CardsContainer/CardsContainer.jsx
@@ -1,10 +1,49 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../../services/firebase";
 import { Card } from "../Card/Card"
 
 export const CardsContainer = () => {
+  const usersCollectionRef = collection(db, "users");
+  const [users, setUsers] = useState({})
+  const [firestoreLoading, setFirestoreLoading] = useState(true);
+  // console.log(users)
+
+  // Users Data
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    }
+    getUsers();
+  }, [])
+
+   // Firestore loading
+  const [value, loading, error] = useCollection(usersCollectionRef,
+    { snapshotListenOptions: { includeMetadataChanges: true } }
+  );
+  useEffect(() => {
+    setFirestoreLoading(loading);
+  }, [loading])
+
   return (
     <div className="cards-container-container">
-      <Card />
-      <Card />
+      { 
+        firestoreLoading ?
+        console.log("loading") :
+        users?.map((user) => {
+          return (
+            <Card
+              key={user.id}
+              displayName={user.displayName}
+              phone={user.phone}
+              category={user.size}
+              covered={user.isCovered}
+            />
+          )
+        })
+      }
     </div>
   )
 }
