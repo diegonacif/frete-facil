008afe640bf73beab9baa5505752000e9e8dc01f
diegonacif <diegoreisnacif@gmail.com>
2023-02-04 00:23:54 -0300

    style: login style

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthGoogleContext } from "../../contexts/AuthGoogleProvider";
+import { AiFillGoogleCircle, AiFillCloseCircle } from 'react-icons/ai';
 import "../../css/App.css";
 
 export const Login = () => {
@@ -29,9 +30,19 @@ export const Login = () => {
 
   return (
     <div className="login-container">
-      <span>Cadastre-se com sua conta Google</span>
-      <button onClick={() => loginGoogle()}>Google</button>
-      <button onClick={() => logoutGoogle()}>Logout</button>
+      <header>
+        <h2>Área do Freteiro</h2>
+        <Link to="/">
+          <AiFillCloseCircle size="2rem" />
+        </Link>
+      </header>
+      <div className="login-main">
+        <h3>Faça login com sua conta Google</h3>
+        <div className="login-button" onClick={() => loginGoogle()}>
+          <AiFillGoogleCircle size="1.5rem"/>
+          <span>Continuar com Google</span>
+        </div>
+      </div>
     </div>
   )
 }
